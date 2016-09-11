c9ede9b6a97d97bc11789fa74d6e481761fdb39b
Slanf <1147649695@qq.com>
2016-09-11 14:32:22 +0800

    leetcode

--- src/main/java/LeetCode.java
+++ src/main/java/LeetCode.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Song on 2016/7/29.
@@ -7,19 +8,43 @@ public class LeetCode {
     private static int value = 2;
 
     public static void main(String [] args){
-        int [] nums1 = {1,4,6,9};
-        int [] nums2 = {2,6,10,11};
+        int [] nums1 = {100001};
+        int [] nums2 = {};
         LeetCode lc = new LeetCode();
         System.out.println(lc.findMedianSortedArrays(nums1,nums2));
- //       System.out.println(guessNumber(2));
     }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
+        int temp2=0;  //记录nums2中的上次插入位置
         ArrayList<Integer> res = new ArrayList<Integer>();
-
-        if(0==(len1+len2)%2) return (res.get((len1+len2)/2-1)+res.get((len1+len2)/2+1))/2.0;
-        else return res.get((len1+len2)/2+1);
+        //将nums1向nums2中插入
+        for(int i=0;i<len1;i++){
+            for(int j=temp2;j<len2;j++){
+                if(nums1[i]<nums2[j]) {
+                    res.add(nums1[i]);
+                    temp2=j;
+                    break;
+                }else{
+                    res.add(nums2[j]);
+                    if(j>=len2-1) {
+                        temp2 = len2;
+                    }
+                }
+            }
+            if(temp2>=len2){
+                res.add(nums1[i]);
+            }
+        }
+        if(temp2<len2){//nums2中还有元素未被插入
+            for(int i=temp2;i<len2;i++){
+                res.add(nums2[i]);
+            }
+        }
+        if(0==(len1+len2)%2)
+            return (res.get((len1+len2)/2-1)+res.get((len1+len2)/2))/2.0;
+        else
+            return res.get((len1+len2)/2);
     }
 
 }
